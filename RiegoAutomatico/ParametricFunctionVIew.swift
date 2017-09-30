bdc2263675fc5bcfaca9105bddc76323d3772326
Sofia Vidal Urriza <sofiavidal@Sofias-Mac.local>
2017-09-30 19:58:08 +0200

    RiegoAutomaticoV2

--- RiegoAutomatico/ParametricFunctionVIew.swift
+++ RiegoAutomatico/ParametricFunctionVIew.swift
@@ -9,12 +9,13 @@
 import UIKit
 
 protocol ParametricFunctionViewDataSource{
-    func start() -> Double
-    func end() -> Double
-    func pintAt(index : Double) -> Double
+    func startFor(_ pfgv : ParametricFunctionVIew) -> Double
+    func endFor(_ pfgv : ParametricFunctionVIew) -> Double
+    func nextPointAt(_ pfgv : ParametricFunctionVIew, poinAt index: Double) -> FunctionPoint
+    
 }
 
-struct FunctionPint {
+struct FunctionPoint {
     var x : Double = 0.0
     var y : Double = 0.0
 }
@@ -23,7 +24,71 @@ class ParametricFunctionVIew: UIView {
     
     // Objeto que comunica esta clase con ViewController
     var dataSource : ParametricFunctionViewDataSource!
+    var scaleX = 1.0
+    var scaleY = 1.0
     
-
+    override func draw(_ rect: CGRect) {
+        drawAxis()
+        drawTrajectory()
+    }
+    
+    private func drawAxis(){
+        let w = bounds.width
+        let h = bounds.height
+        
+        // Pintamos eje x
+        let pathX = UIBezierPath()
+        pathX.move(to: CGPoint(x: 0, y: h/2))
+        pathX.addLine(to: CGPoint(x: w, y: h/2))
+        pathX.lineWidth = 2
+        UIColor.black.setStroke()
+        pathX.stroke()
+        
+        // Pintamos eje Y
+        let pathY = UIBezierPath()
+        pathY.move(to: CGPoint(x: w/2, y: 0.0))
+        pathY.addLine(to: CGPoint(x: w/2, y: h))
+        pathY.lineWidth = 2
+        UIColor.black.setStroke()
+        pathY.stroke()
+        
+    }
+    
+    private func drawTrajectory(){
+        //path vacio
+        let path = UIBezierPath()
+        
+        let w = bounds.width
+        let h = bounds.height
+        
+        path.move(to: CGPoint(x: w/2, y: h/2))
+        
+        let Pi = dataSource.startFor(self)
+        let Pf = dataSource.endFor(self)
+        
+        for p in stride(from: Pi, to: Pf, by: 0.1){
+        
+            let v = dataSource.nextPointAt(self, poinAt: p)
+            let x = scalingX(v.x)
+            let y = scalingY(v.y)
+            
+            //Actualizo la linea
+            path.addLine(to: CGPoint(x: x, y: y))
+        }
+        //Trazo
+        path.lineWidth = 1.5
+        UIColor.red.setStroke()
+        path.stroke()
+    }
+    
+    private func scalingX(_ scaleInX: Double) -> Double {
+        let weigh = Double(bounds.width)
+        return (weigh/2 + scaleInX*scaleX)
+    }
+    
+    private func scalingY(_ scaleInY: Double) -> Double {
+        let height = Double(bounds.height)
+        return (height/2 + scaleInY*scaleY)
+    }
     
 }
--- RiegoAutomatico/ViewController.swift
+++ RiegoAutomatico/ViewController.swift
@@ -8,17 +8,80 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, ParametricFunctionViewDataSource {
+    func startFor(_ pfgv: ParametricFunctionVIew) -> Double {
+        return 0.0
+    }
+    
+    func endFor(_ pfgv: ParametricFunctionVIew) -> Double {
+        return 200.0
+    }
+    
+    func nextPointAt(_ pfgv: ParametricFunctionVIew, poinAt index: Double) -> FunctionPoint {
+        // en esta funcion solo le dices que pinte una x y una y, pero aun no sabes que van a ser
+        // es en el ViewController con un swich case cuando le dices x e y en funcion de la grafica
+        switch pfgv {
+        case graph1:
+            return FunctionPoint(x: index, y: sin(index))
+        case graph2:
+            return FunctionPoint(x: index, y: cos(index))
+        case graph3:
+            return FunctionPoint(x: index, y: tan(index))
+        default:
+            return FunctionPoint(x: 0.0, y: 0.0)
+        }
+    }
+    
+    
+    
+    @IBOutlet weak var graph1: ParametricFunctionVIew!
+    
+    @IBOutlet weak var graph2: ParametricFunctionVIew!
+    
+    @IBOutlet weak var graph3: ParametricFunctionVIew!
+    // para variar el tiempo
+    @IBOutlet weak var tSlider: UISlider!
+    // falta otro slider con otro param
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Conexion entre clases
+        graph1.dataSource = self
+        graph2.dataSource = self
+        graph3.dataSource = self
+        
+        // Escalado
+        // x con 1 se ve muy juntas las sinusoides
+        // y *100 para que se vean los picos bien
+        
+        graph1.scaleX = 10.0
+        graph1.scaleY = Double(tSlider.value*100)
+        
+        graph2.scaleX = 10.0
+        graph2.scaleY = Double(tSlider.value*100)
+        
+        graph3.scaleX = 10.0
+        graph3.scaleY = Double(tSlider.value*50)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // Control del Slider
+    @IBAction func sliderScale(_ sender: UISlider) {
+        graph1.scaleY = Double(tSlider.value*100)
+        graph1.setNeedsDisplay()
+        
+        graph2.scaleY = Double(tSlider.value*100)
+        graph2.setNeedsDisplay()
+        
+        graph3.scaleY = Double(tSlider.value*100)
+        graph3.setNeedsDisplay()
+    }
+    
 
 
 }
