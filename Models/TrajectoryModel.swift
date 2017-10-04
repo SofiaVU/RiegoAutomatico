8a8dd84e54b71c6d89b4c80cb066eda183caf241
Sofia Vidal Urriza <sofiavidal@Sofias-Mac.local>
2017-10-04 18:09:58 +0200

    RiegoAutomaticoV3

---
+++ Models/TrajectoryModel.swift
@@ -0,0 +1,9 @@
+//
+//  TrajectoryModel.swift
+//  RiegoAutomatico
+//
+//  Created by Sofia Vidal Urriza on 30/09/2017.
+//  Copyright © 2017 Sofia Vidal Urriza. All rights reserved.
+//
+
+import Foundation
